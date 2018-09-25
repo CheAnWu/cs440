5eb63c27561899c8601ad381a2cb9a083b5c967c
mlemacio <mlemacio0@gmail.com>
2018-09-24 21:37:35 -0500

    hk

--- search.py
+++ search.py
@@ -21,18 +21,23 @@ files and classes when code is run, so be careful to not modify anything else.
 
 from queue import *
 
-def makeTrueDistChart(maze, goals):
+def makeTrueDistChart(maze, goals, start):
     goalsAndStart = goals.copy()
-    goalsAndStart.insert(0, maze.getStart())
-    fullChart = [[]]
+    goalsAndStart.insert(0, start)
+    fullChart = [[0 for x in range(0, len(goalsAndStart))] for x in range(0, len(goalsAndStart))]
+
     for i in range(len(goalsAndStart)):
 
         for j in range(i,len(goalsAndStart)):
-            if i == j:
+
+            if (i == j):
                 fullChart[i][j] = 0
-            value = preComputationAstar(maze, goals[i], goals[j])
-            fullChart[i][j] = value
-            fullChart[j][i] = value
+            else:
+                value = preComputationAstar(maze, goals[i-1], goals[j-1])
+                #print(goals[i-1], goals[j-1], value)
+                fullChart[i][j] = value
+                fullChart[j][i] = value
+
     return fullChart
 
 
@@ -70,7 +75,7 @@ def preComputationAstar(maze, start, end):
             else:
                 fScore[(i, j)] = 10000000
 
-    fScore[start] = a_star_heuristic(start, end)
+    fScore[start] = distance_away(start, end)
 
     open_set.put((fScore[start], start))
     track_set.add(start)
@@ -82,7 +87,7 @@ def preComputationAstar(maze, start, end):
         track_set.remove(current)
 
         if (current == end):
-            return len(came_from)
+            return len(make_path(current, came_from))
 
         closed_set.add(current)
 
@@ -96,7 +101,7 @@ def preComputationAstar(maze, start, end):
 
                 came_from[neighbor] = current
                 gScore[neighbor] = tentative_gScore
-                fScore[neighbor] = gScore[neighbor] + a_star_heuristic(neighbor, end)
+                fScore[neighbor] = gScore[neighbor] + distance_away(neighbor, end)
 
                 open_set.put((fScore[neighbor], neighbor))
                 track_set.add(neighbor)
@@ -106,7 +111,7 @@ def preComputationAstar(maze, start, end):
 
             came_from[neighbor] = current
             gScore[neighbor] = tentative_gScore
-            fScore[neighbor] = gScore[neighbor] + a_star_heuristic(neighbor, end)
+            fScore[neighbor] = gScore[neighbor] + distance_away(neighbor,end)
 
     return 100000
 
@@ -324,7 +329,7 @@ def greedy(maze):
     meta[start] = None
     spot_information[start] = (0, distance_away(start, end), 0)
 
-    print(start)
+    #print(start)
 
     open_set.put( (spot_information[start][0] + spot_information[start][1], start) )
 
@@ -364,11 +369,64 @@ def greedy(maze):
     #coordinate
     return [], num_states_explored
 
+
+def longest_length_2(graph, leng, root):
+
+    lengths = []
+
+    #print(graph)
+    flag = True
+    for i in graph:
+        #print(i)
+        if(i[0] == root):
+            flag = False
+            graph.remove(i)
+            sum = i[2] + longest_length_2(graph, i[2], i[1])
+            lengths.append(sum)
+            #print(lengths)
+        elif(i[1] == root):
+            flag = False
+            graph.remove(i)
+            sum = i[2] + longest_length_2(graph, i[2], i[0])
+            lengths.append(sum)
+            #print(lengths)
+
+
+    if(flag):
+        return 0
+
+    if(root == 0):
+        max1 = max(lengths)
+        lengths.remove(max1)
+        if(len(lengths) == 0):
+            return max1
+        max2 = max(lengths)
+        return max1+ max2
+
+    return (max(lengths))
+
+
+
+
+
 def a_star_heuristic(spot, goals, maze):
     #Who knows
     #LUKASKNOWS
 
+    '''
+    chart = makeTrueDistChart(maze, goals.copy(), spot)
 
+
+    rep_graph = Graph(len(goals) + 1)
+
+    for i in range(0, len(chart)):
+        for j in range(i, len(chart[0])):
+            rep_graph.addEdge(i, j, chart[i][j])
+    Kruskal_findMST(rep_graph)
+    baby = longest_length_2(rep_graph.graph[len(goals)+1:len(goals)+1 + len(goals)].copy(), 0, 0 )
+    #print (baby)
+    return baby - len(goals)
+    '''
 
 
 
@@ -427,7 +485,7 @@ def a_star_heuristic(spot, goals, maze):
     else:
         top_bot_dist = abs(x_coord[0] - top_most[0]) + abs(x_coord[0] - bot_most[0])*2
 
-    return left_right_dist + top_bot_dist
+    return 3*(left_right_dist + top_bot_dist)
 
 def astar(maze):
 
@@ -461,6 +519,17 @@ def astar(maze):
 
     goals = maze.getObjectives()
 
+    chart = makeTrueDistChart(maze, goals, maze.getStart())
+
+    rep_graph = Graph(len(goals) + 1)
+
+    for i in range(0, len(chart)):
+        for j in range(i, len(chart[0])):
+            rep_graph.addEdge(i, j, chart[i][j])
+
+    Kruskal_findMST(rep_graph)
+    #print(rep_graph.graph)
+
     fScore[start] = a_star_heuristic(start, goals, maze)
 
     open_set.put( (fScore[start], [start], goals.copy(), set() ) )
@@ -472,12 +541,14 @@ def astar(maze):
 
         current_path = current_tuple[1]
         current_goals = current_tuple[2]
+
+        print("goals:", current_goals)
         visited = current_tuple[3]
 
         last_spot = current_path[-1]
 
-        print(current_path)
-        print("Goals: ", current_goals)
+        #print(current_path)
+        #print("Goals: ", current_goals)
 
         num_states_explored += 1
 
@@ -507,3 +578,76 @@ def astar(maze):
             open_set.put( (h_value, neighbor_path, neighbor_goals, neighbor_visited ) )
 
     return [], 0
+
+#Obtained from https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
+class Graph:
+
+    def __init__(self,vertices):
+        self.V= vertices #No. of vertices
+        self.graph = [] # default dictionary
+                                # to store graph
+
+
+    # function to add an edge to graph
+    def addEdge(self,u,v,w):
+        self.graph.append([u,v,w])
+
+    # A utility function to find set of an element i
+    # (uses path compression technique)
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    # A function that does union of two sets of x and y
+    # (uses union by rank)
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        # Attach smaller rank tree under root of
+        # high rank tree (Union by Rank)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+
+        # If ranks are same, then make one as root
+        # and increment its rank by one
+        else :
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+def Kruskal_findMST(input_graph):
+
+    final_MST = []
+
+    index = 0
+    e_index = 0
+
+    input_graph.graph = sorted(input_graph.graph, key=lambda item: item[2])
+
+    parent = []
+
+    rank = []
+
+    for node in range(input_graph.V):
+        parent.append(node)
+        rank.append(0)
+
+    while (e_index < input_graph.V - 1):
+        u,v,w = input_graph.graph[index]
+        index = index + 1
+        x = input_graph.find(parent, u)
+        y = input_graph.find(parent, v)
+
+
+        if (x != y):
+            e_index += 1
+            final_MST.append([u,v,w])
+            input_graph.union(parent, rank, x, y)
+    # print the contents of result[] to display the built MST
+    #print ("Following are the edges in the constructed MST")
+    #for u,v,weight in final_MST:
+        #print str(u) + " -- " + str(v) + " == " + str(weight)
+        #print ("%d -- %d == %d" % (u,v,weight))
