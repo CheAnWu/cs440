545bbef32089670a7e7940dfdf8328ce4ebaa53a
Lukas Kulbis <lukaskulbis@gmail.com>
2018-10-19 16:30:54 -0500

    Got our probs list

--- Mp4/naive_bayes.py
+++ Mp4/naive_bayes.py
@@ -12,6 +12,39 @@ This is the main entry point for MP4. You should only modify code
 within this file -- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
+import numpy as np
+
+
+def parseIntoWordList(train_set, train_labels, isSpam):
+    wordList = []
+    wordCount = []
+
+    for i in range(len(train_labels)):
+        if (train_labels[i] != isSpam):
+            continue
+        curr_email = train_set[i]
+        for word in curr_email:
+            if word in wordList:
+                wordCount[wordList.index(word)] += 1
+            else:
+                wordList.append(word)
+                wordCount.append(1)
+
+    return wordList, wordCount
+
+
+# creates list of words with corresponding number = ('cat'in spam emails / all words in spam emails)
+def createProbabilitiesList(train_set, train_labels, isSpam):
+    words, wordcount = parseIntoWordList(train_set, train_labels, isSpam)
+
+    problist = []
+    denominator = sum(wordcount)
+
+    for i in range(len(words)):
+        problist[i] = wordcount[i] / denominator
+
+    return words, problist
+
 
 def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter):
     """
@@ -28,10 +61,14 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter):
 
     smoothing_parameter - The smoothing parameter you provided with --laplace (1.0 by default)
     """
+
     # TODO: Write your code here
     # return predicted labels of development set
 
-    
+    spamWords, spamProbs = createProbabilitiesList(train_set, train_labels, 1)
+    hamWords, hamProbs = createProbabilitiesList(train_set, train_labels, 0)
 
+    loggedSpam = np.log(spamProbs)
+    loggedHam = np.log(hamProbs)
 
     return []
