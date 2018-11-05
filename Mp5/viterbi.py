0a1c358fd78f387c979107aea493d0d70895bf74
Lukas Kulbis <lukaskulbis@gmail.com>
2018-11-05 17:27:08 -0600

    Completed Initial probabilities and Transition Matrix

--- Mp5/viterbi.py
+++ Mp5/viterbi.py
@@ -20,6 +20,10 @@ input:  training data (list of sentences, with tags on the words)
 output: list of sentences, each sentence is a list of (word,tag) pairs. 
         E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
 '''
+
+import numpy as np
+
+
 def baseline(train, test):
     predicts = []
     word_to_tag_counts = {}
@@ -63,5 +67,89 @@ output: list of sentences with tags on the words
         E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
 '''
 def viterbi(train, test):
+    word_to_tag_counts = {}
+    tag_totals = {}
+
+    tag_index = {}
+    index_count = 0
+
+    for sentence in train:
+        for pair in sentence:
+            word, tag = pair
+
+            if tag in tag_totals:
+                tag_totals[tag] += 1
+            else:
+                tag_totals[tag] = 1
+                tag_index[tag] = index_count
+                index_count += 1
+
+            if word not in word_to_tag_counts:
+                word_to_tag_counts[word] = {}
+            if tag in word_to_tag_counts[word]:
+                word_to_tag_counts[word][tag] += 1
+            else:
+                word_to_tag_counts[word][tag] = 1
+
+# tag count per word/ total tag count - emission probability
+    for key, value in word_to_tag_counts.items():
+        word_to_tag_counts[key][value] = word_to_tag_counts[key][value]/tag_totals[value]
+
+
+    initial_tag_probabilities = np.zeros(len(index_count))
+    transition_matrix = np.zeros(shape = (len(index_count), len(index_count)))
+
+
+    for sentence in train:
+        first = True
+        for i in sentence[:-1]:
+            word, tag = sentence[i]
+
+            curr_tag_idx = tag_index[tag]
+
+            if first:
+                initial_tag_probabilities[curr_tag_idx] += 1
+                first = False
+
+            else:
+                next_tag = sentence[i+1][1]
+                transition_matrix[curr_tag_idx][tag_index[next_tag]] += 1
+
+
+
+    #Do we log this here?
+    #count tag starts sentence / total num sentences
+    for i in initial_tag_probabilities:
+        initial_tag_probabilities[i] = initial_tag_probabilities[i]/len(train)
+
+
+
+    #LaPlace smoothing: (1+transition occurances)/(tag occurences + num tags)
+    for tag, count in tag_totals.items():
+        curr_idx = tag_index[tag]
+        for i in range(len(transition_matrix)):
+            transition_matrix[curr_idx][i] = (transition_matrix[curr_idx][i] + 1)/ (count + len(tag_totals))
+
+    print(initial_tag_probabilities)
+    print(transition_matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     predicts = []
     return predicts
