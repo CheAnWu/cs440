1fcb3e7057b0b8bcb3d7a35a7ad95d7a26adbf71
Lukas Kulbis <lukaskulbis@gmail.com>
2018-12-11 02:53:38 -0600

    0.4 bounces

--- Mp7/agent.py
+++ Mp7/agent.py
@@ -2,10 +2,17 @@ import utils
 import numpy as np
 import math
 import random
+from random import randint
 
+
+action = [-1, 0, 1]
+
+LEARNING_CONSTANT = .4
+EPSILON = LEARNING_CONSTANT
+DISCOUNT_RATE = 0.7
 
 def discretState(state):
-    new = np.zeros(len(state), dtype=int)
+    new = np.zeros(len(state), dtype= int)
     new[0] = int(math.floor(state[0] * 12))
     if new[0] > 11:
         new[0] = int(11)
@@ -14,9 +21,9 @@ def discretState(state):
         new[1] = int(11)
 
     if state[2] > 0:
-        new[2] = int(1)
+        new[2] = int(0)
     else:
-        new[2] = int(2)
+        new[2] = int(1)
 
     if np.absolute(state[3]) < 0.015:
         new[3] = int(0)
@@ -25,7 +32,7 @@ def discretState(state):
     else:
         new[3] = int(2)
 
-    new[4] = int(math.floor(state[4] * 15))  # 15 because the range for paddle is [0, 0.8]
+    new[4] = int(math.floor(state[4] * 15)) #15 because the range for paddle is [0, 0.8]
     if new[4] > 11:
         new[4] = int(11)
 
@@ -44,42 +51,70 @@ def get_reward(self, bounces, done, won):
     return 0
 
 
+
+
 def get_val(matrix, state):
     return matrix[state[0]][state[1]][state[2]][state[3]][state[4]]
 
 
+
 class Agent:
+
     def choose_action(self, discret_state):
-        print(discret_state)
-        print(type(discret_state[0]))
-        print(self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][0])
-        print(self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][1])
-        print(self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][2])
-
-        qActions = self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][
-            discret_state[4]].copy()
-
-        maxQ = max(qActions)
-
         if random.random() < self.learning_rate:
-            best = [i for i in range(len(self._actions)) if qActions[i] == maxQ]
-            i = random.choice(best)
+            return randint(0,2)
         else:
-            i = random.choice(np.where(qActions == maxQ))
+            maxVal, idx = self.getMax(discret_state)
+            return idx
 
-        # action = self.actions[i]
-        action = random.randint(0, 2)
-        print(action)
-        return action
+    def getMax(self, state):
+        qVals = self.Q[state[0]][state[1]][state[2]][state[3]][state[4]]
+        bigVal = np.argmax(qVals)
+        idx = []
+        for i in range(3):
+            if qVals[i] == bigVal:
+                idx.append(i)
 
-    def __init__(self, actions, two_sided=False):
-        self.old_state = None
-        self.old_reward = None
-        self.old_action = None
+        if len(idx) > 0:
+            index = random.choice(idx)
+            return bigVal, index
+        else:
+            return qVals[1], 1
+
+
+    def updateQ(self, state):
+        max_newQ, idx = self.getMax(state)
+
+        oldQ = self.Q[self.old_state[0]][self.old_state[1]][self.old_state[2]][self.old_state[3]][self.old_state[4]][
+            self.old_action]
+
+
+        n = self.N[state[0]][state[1]][state[2]][state[3]][
+            state[4]][self.old_action]
+
+
+        val = self.learning_rate * (1/self.N[state[0]][state[1]][state[2]][state[3]][
+            state[4]][self.old_action]) * (self.old_reward + self.discount_rate * max_newQ - oldQ)
+
+        return self
+
+    def updateRates(self):
+        n = self.N[self.old_state[0]][self.old_state[1]][self.old_state[2]][self.old_state[3]][
+            self.old_state[4]][self.old_action]
+
+        self.learning_rate = LEARNING_CONSTANT / (LEARNING_CONSTANT + n)
+
+        self.discount_rate = DISCOUNT_RATE / (DISCOUNT_RATE + n)
+
+
+
+    def __init__(self, actions, two_sided = False):
+        self.old_state = [-1,-1,-1,-1,-1]
+        self.old_reward = 0
+        self.old_action = 1
         self.max_bounces = 0
-        self.learning_rate = .2  # will need to update
-        # rate of decay is C/(C + N(s,a))
-        self.discount_rate = .2  # need to update
+        self.learning_rate = LEARNING_CONSTANT / (LEARNING_CONSTANT + 1)
+        self.discount_rate = DISCOUNT_RATE / (DISCOUNT_RATE + 1)
         self.two_sided = two_sided
         self._actions = actions
         self._train = True
@@ -93,90 +128,70 @@ class Agent:
         self.Q = utils.create_q_table()
         self.N = utils.create_q_table()
 
+
     def act(self, state, bounces, done, won):
-        if not self._train:
-            if done:
-                return 0
 
         discret_state = discretState(state)
 
-        # Do we only need this for train
+        if not self._train:
+            if done:
+                return 0
+            maxVal, maxidx = self.getMax(discret_state)
+            return action[maxidx]
 
-
-        # if terminal then update q with new r
-
-
-
-        if self._train:  # explore and exploit
-            # increment N[s, a]
-            # Update Q[s,a] <-- Q[s,a] + learning*N[s,a] * (r + discount * max Q[s', a'] - Q[s,a])
-            # self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]]
-
+        else:
             reward = 0
             if done:
                 if won:
                     reward = 1
                 else:
                     reward = -1
-                self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][self.old_action] = reward
-                self.old_action = 0
-                self.old_state[0] = -1
+
+                self.max_bounces = 0
+                self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][self.old_action] = reward
+                self.old_action = 1
+                #self.old_state[0] = -1
                 self.old_reward = 0
-                return 0
+                return action[1]
+
 
             if bounces > self.max_bounces:
                 self.max_bounces = bounces
                 reward = 1
 
-            new_action = self.choose_action(discret_state)
-            if self.old_state[0] is not None:
-                self.old_action = new_action
+            if self.old_state[0] == -1:
+                self.old_action = 1
                 self.old_state = discret_state.copy()
                 self.old_reward = reward
-                return new_action
+                return action[1]
 
-            max = self.Q[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][
-                      new_action] - \
-                  self.Q[self.old_state[0]][self.old_state[1]][self.old_state[2]][self.old_state[3]][self.old_state[4]][
-                      self.old_action]
+            self.N[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][self.old_action] += 1
+            self.updateRates()
+            new_action_idx = self.choose_action(discret_state)
 
-            alpha = self.learning_rate / (self.learning_rate + self.N[self.old_state[0]][self.old_state[1]][self.old_state[2]][self.old_state[3]][self.old_state[4]][self.old_action])
-
-            self.N[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][discret_state[4]][
-                self.old_action] += 1
-            self.Q[self.old_state[0]][self.old_state[1]][self.old_state[2]][self.old_state[3]][self.old_state[4]][
-                self.old_action] += alpha * \
-                                    self.N[discret_state[0]][discret_state[1]][discret_state[2]][discret_state[3]][
-                                        discret_state[4]][self.old_action] * (
-                                    self.old_reward + self.discount_rate * max)
+            self.updateQ(discret_state)
 
             self.old_state = discret_state
             self.old_reward = reward
-            self.old_action = new_action
+            self.old_action = new_action_idx
 
-            # print(type(int(new_action)))
-            return int(new_action)
+            return int(action[new_action_idx])
 
-
-            # update Q table and return best action
-        else:
-            # exploit
-            print("testing")
-            return 0
-
-        print(type(self._actions[0]))
         return self._actions[0]
 
     def train(self):
         self._train = True
-
+        
     def eval(self):
         self._train = False
 
-    def save_model(self, model_path):
+    def save_model(self,model_path):
         # At the end of training save the trained model
-        utils.save(model_path, self.Q)
+        utils.save(model_path,self.Q)
 
-    def load_model(self, model_path):
+    def load_model(self,model_path):
         # Load the trained model for evaluation
         self.Q = utils.load(model_path)
+
+
+
